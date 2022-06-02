f1e2b74254c38e50511ead503d4749238a675348
Argo-Lenovo <argo@163.com>
2022-06-02 17:50:32 +0800

    test: 增加 Mock 对象

---
+++ test/UnitTest/Extensions/LambadaExtensionsTest.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Shared;
+using System.Linq;
+
+namespace UnitTest.Extensions;
+
+public class LambadaExtensionsTest
+{
+    [Fact]
+    public void GetFilterFunc_Null()
+    {
+        var foos = new Foo[]
+        {
+            new() { Count = 1 },
+            new() { Count = 2 },
+            new() { Count = 10 },
+            new() { Count = 11 }
+        };
+        var filter = Array.Empty<MockFilterActionBase>();
+        var items = foos.Where(filter.GetFilterFunc<Foo>());
+        Assert.Equal(4, items.Count());
+    }
+
+
+    private abstract class MockFilterActionBase : IFilterAction
+    {
+        public abstract IEnumerable<FilterKeyValueAction> GetFilterConditions();
+
+        public virtual void Reset()
+        {
+        }
+
+        public virtual Task SetFilterConditionsAsync(IEnumerable<FilterKeyValueAction> conditions)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    private class MockAndFilterAction1 : MockFilterActionBase
+    {
+        public override IEnumerable<FilterKeyValueAction> GetFilterConditions()
+        {
+            var filters = new FilterKeyValueAction[]
+            {
+                new()
+                {
+                     FieldKey = "Count",
+                     FieldValue = 1,
+                     FilterAction = FilterAction.GreaterThan,
+                     FilterLogic = FilterLogic.And
+                },
+                new()
+                {
+                     FieldKey = "Count",
+                     FieldValue = 10,
+                     FilterAction = FilterAction.LessThan
+                }
+            };
+            return filters;
+        }
+    }
+
+    private class MockAndFilterAction2 : MockFilterActionBase
+    {
+        public override IEnumerable<FilterKeyValueAction> GetFilterConditions()
+        {
+            var filters = new FilterKeyValueAction[]
+            {
+                new()
+                {
+                     FieldKey = "Count",
+                     FieldValue = 2,
+                     FilterAction = FilterAction.Equal,
+                     FilterLogic = FilterLogic.And
+                }
+            };
+            return filters;
+        }
+    }
+
+    private class MockOrFilterAction1 : MockFilterActionBase
+    {
+        public override IEnumerable<FilterKeyValueAction> GetFilterConditions()
+        {
+            var filters = new FilterKeyValueAction[]
+            {
+                new()
+                {
+                     FieldKey = "Count",
+                     FieldValue = 1,
+                     FilterAction = FilterAction.Equal,
+                     FilterLogic = FilterLogic.Or
+                },
+                new()
+                {
+                     FieldKey = "Count",
+                     FieldValue = 2,
+                     FilterAction = FilterAction.Equal,
+                     FilterLogic = FilterLogic.Or
+                }
+            };
+            return filters;
+        }
+    }
+
+    private class MockOrFilterAction2 : MockFilterActionBase
+    {
+        public override IEnumerable<FilterKeyValueAction> GetFilterConditions()
+        {
+            var filters = new FilterKeyValueAction[]
+            {
+                new()
+                {
+                     FieldKey = "Count",
+                     FieldValue = 10,
+                     FilterAction = FilterAction.Equal
+                }
+            };
+            return filters;
+        }
+    }
+}
